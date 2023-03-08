7a62a154ed9a4e1f9be4af482afeb25353109ed4
sachin <sachin@gmail.com>
2023-03-08 21:02:48 +0530

    Password hashing script update 2

--- Models/register.model.js
+++ Models/register.model.js
@@ -24,19 +24,19 @@ register.statics.registerUser = async function (
   password,
   confirmPassword
 ) {
-  const exists = await this.findOne({ userName });
-  if (exists) throw Error("User already exists");
+  //   const exists = await this.findOne({ userName });
+  //   if (exists) throw Error("User already exists");
 
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(password, salt);
 
-  const user = await this.create({
-    userName,
-    password: hash,
-    confirmPassword: hash,
-  });
+  //   const user = await this.create({
+  //     userName,
+  //     password: hash,
+  //     confirmPassword: hash,
+  //   });
 
-  return [user.username, user.password, user.confirmPassword];
+  return [userName, hash, confirmPassword];
 };
 
 module.exports = mongoose.model("register", register);
